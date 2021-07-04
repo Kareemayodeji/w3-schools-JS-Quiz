67d04effaf0efa23d96204f1c1844e78b232d96b
Kareem Ayodeji <kareem.ayodeji47@gmail.com>
2021-07-04 22:23:41 +0200

    Complete Project
    Completed all functionality using JavaScript.

---
+++ w3 schools JS Quiz/js/questions.js
@@ -0,0 +1,58 @@
+//Creating an array and passing the  number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Inside which HTML element do we put the JavaScript?",
+        answer: "script",
+        options: [
+            "script",
+            "js",
+            "javascript",
+            "scripting"
+        ]
+    },
+    {
+        numb: 2,
+        question: "How do you create a function in JavaScript?",
+        answer: "function myFunction()",
+        options: [
+            "function:myFunction()",
+            "function = myFunction()",
+            "function myFunction()",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Where is the correct place to insert a JavaScript?",
+        answer: "Both the head section and the body section are correct",
+        options: [
+            "Both the head section and the body section are correct",
+            "The body section",
+            "The head section",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        answer: "script src='xxx.js'",
+        options: [
+            "script name='xxx.js'",
+            "script href='xxx.js'",
+            "script src='xxx.js'",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How do you write 'Hello World' in an alert box?",
+        answer: "alert('Hello World');",
+        options: [
+            "msgBox('Hello World');",
+            "alert('Hello World');",
+            "alertBox('Hello World');",
+            "msg('Hello World');"
+        ]
+    },
+];
